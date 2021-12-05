01e4a1891b84ccda73fa172865eccdb9f8dac9ed
Alexis Flores <afallendes@outlook.com>
2021-12-05 18:53:24 +0000

    Update entrypoint.sh

--- project/entrypoint.sh
+++ project/entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
-python manage.py makemigrations && \
-python manage.py migrate && \
+# Make migrations for admin, auth, contenttypes, sessions apps
+python manage.py makemigrations
+
+# Make migrations for backend app
+python manage.py makemigrations backend
+
+# Migrate models
+python manage.py migrate
+
+# Run development server
 python manage.py runserver 0.0.0.0:8000
