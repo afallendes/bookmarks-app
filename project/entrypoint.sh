1f55f2f00cd9b5f030311cbd73147d23bf2e445b
afallendes <af.allendes@outlook.com>
2021-12-20 16:12:34 +0000

    Add superuser from env

--- project/entrypoint.sh
+++ project/entrypoint.sh
@@ -9,5 +9,8 @@ python manage.py makemigrations backend
 # Migrate models
 python manage.py migrate
 
+# Creates superuser from environment variables
+python manage.py createsuperuser --noinput
+
 # Run development server
 python manage.py runserver 0.0.0.0:8000
