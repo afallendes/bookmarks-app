c2d62c8166078f1e4e8b4bb213a66d2b1e16a919
afallendes <af.allendes@outlook.com>
2021-12-31 01:59:55 +0000

    Fix base.html reference to base.js

--- project/frontend/static/frontend/base.js
+++ project/frontend/static/frontend/base.js
@@ -4,5 +4,4 @@ const $$ = (s) => document.querySelectorAll(s);
 const $$$ = (a) => Array.from(a)
 
 // Apply Bootstrap's form-control class to form's search input
-searchInput = $('#id_search') 
-searchInput.classList.add('form-control')
+$('#id_search').classList.add('form-control')
