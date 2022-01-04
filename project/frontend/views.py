bfd7efbbf736d4bf4a8ffb1d5500311e34523de9
afallendes <af.allendes@outlook.com>
2022-01-04 21:53:21 +0000

    Refactor frontend templates

--- project/frontend/views.py
+++ project/frontend/views.py
@@ -18,6 +18,12 @@ class BaseListView(LoginRequiredMixin, ListView):
         user = self.request.user
         return super().get_queryset().order_by('-created_at').filter(user=user)
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["enable_new_bookmark_button"] = True
+        return context
+    
+
 
 class BookmarkListView(BaseListView):
     """
