962cd855f4a1f03dcb90379ca1548cf423086306
afallendes <af.allendes@outlook.com>
2022-01-20 22:53:12 +0000

    Add bookmak create logic along with new extra tags

--- project/frontend/static/base.js
+++ project/frontend/static/base.js
@@ -1,7 +1,59 @@
-$(document).ready(function() {
-    // Add Bootstrap's form-control class to all visible 'input' elements
+$(function () {
+
+    // Add Bootstrap's form-control class to all visible 'input' elements.
     $('input:not([type=hidden]):not([type=test])').addClass('form-control');
+
+
+    // Add SelectizeJS to main select element for tags.
+    $('select[multiple]#id_tags').selectize({
+        plugins: ["remove_button"],
+        delimiter: ",",
+        persist: false,
+        onInitialize() {
+            this.extraTags = {} // Helper to keep track of current extra Tags
+        },
+        create(text) {
+            return {
+                // add a '**' prefix to the extra tag's slug value as a helper
+                // for the backend to differentiate if a tag has to be created
+                // or already exists.
+                value: '**' + text.slugify(),
+                text: text
+        }
+        },
+        onItemAdd(value, $item) {
+            if (value.startsWith('**')) {
+                // This function only 'value' directly but not its text. It has
+                // to be captured from the DOM $item.
+                let text = $item[0].innerText.slice(0, -2);
+                this.extraTags[value] = text;
+                $("select#extra-tags").append(new Option(
+                    text, `${value}|${text}`, true, true
+                ));
+            }
+        },
+        onItemRemove(value) {
+            let text = this.extraTags[value];
+            // This delete logic is required due to the way the 'value' of the
+            // extra tags is constructed as a joined pair of value/text string.
+            $(`select#extra-tags option[value='${value}|${text}']`).remove();
+            delete this.extraTags[`${value}|${text}`]
+        }
+
+    })
     
-    // Add custom bootstrap-multiselect to all 'select' elements
-    $("select[multiple]").bsMultiSelect();
+
+    String.prototype.slugify = function (separator = "-") {
+        // Converts a string into its slug equivalent.
+        // REF: https://gist.github.com/codeguy/6684588#gistcomment-3974852
+        return this
+            .toString()
+            .normalize('NFD') // splits accented letters into their base letters and the accents
+            .replace(/[\u0300-\u036f]/g, '') // removes all previously splitted accents
+            .toLowerCase()
+            .replace(/[^a-z0-9 -]/g, '') // remove chars that aren't letters, numbers, a single space or '-'
+            .trim()
+            .replace(/\s+/g, separator);
+    };
+
 });
--- project/frontend/views.py
+++ project/frontend/views.py
@@ -1,3 +1,4 @@
+from email.mime import text
 from string import ascii_uppercase
 from django.db import models
 
@@ -107,6 +108,54 @@ class BookmarkCreateView(CreateView):
     fields = ('title', 'url', 'tags',)
     success_url = reverse_lazy('frontend:bookmarks-list')
 
+    extra_tags = None
+
+    def get_form_kwargs(self):
+        form_kwargs = super().get_form_kwargs()
+        if 'data' in form_kwargs:
+            data = form_kwargs['data'].copy()
+            
+            # Capture new tags passed from form 'extra-tags' field
+            extras = {}
+            for _ in data.getlist('extra-tags'):
+                k, v = _.split('|')
+                k = k[2:] # remove '**' helper at the start of the string
+                extras.update({k: v})
+            self.extra_tags = extras
+            
+            # Remove already captured extra-tags from the 'tags' data. This
+            # is to allow the form instance to properly passed as valid. Any 
+            # options that are not part of the original options will make
+            # validation fail.
+            pks = []
+            for _ in data.getlist('tags'):
+                if not _.startswith('**'):
+                    pks.append(_)
+            
+            data.setlist('tags', pks)
+
+            form_kwargs['data'] = data
+        return form_kwargs
+
+
     def form_valid(self, form):
-        form.instance.user = self.request.user
-        return super().form_valid(form)
+        user = self.request.user
+        
+        form.instance.user = user
+
+        # need to run form_valid() first to create/save form instance, and then
+        # attach the extra tags. This is required due to m-2-m implementation.
+        # REF: https://docs.djangoproject.com/en/3.2/topics/db/examples/many_to_many/
+        
+        form_valid_return = super().form_valid(form)
+
+        bookmark = self.object # tried with get_object() but didn't work
+        extra_tags = self.extra_tags
+        if extra_tags:
+            for slug, text  in extra_tags.items():
+                tag = Tag.objects.create(user=bookmark.user, slug=slug, text=text)
+                bookmark.tags.add(tag)
+        
+        return form_valid_return
+    
+    
