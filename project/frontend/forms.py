313928cd10f1309522293461f6f35eb9949165eb
afallendes <af.allendes@outlook.com>
2022-01-25 18:54:24 +0000

    Update frontend/forms.py ref comments

--- project/frontend/forms.py
+++ project/frontend/forms.py
@@ -6,7 +6,8 @@ class BookmarkForm(forms.ModelForm):
 
     # This custom bookmark form is required to be able to filter tags choices
     # by their corresponding current user. A 'user' value is passed from the
-    # Create/Edit views to the form kwargs.
+    # create/edit views to the form kwargs.
+    # REF: https://forum.djangoproject.com/t/altering-queryset-in-a-field-of-generic-createview-form/4737/7
 
     def __init__(self, *args, **kwargs):
         user = kwargs.pop('user') # This needs to be captured before __init__
