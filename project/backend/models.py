33ece857988443e50e33664fb47d89870b80f72a
afallendes <af.allendes@outlook.com>
2022-01-28 20:41:14 +0000

    Fix bug when create bookmarks w/o icon

--- project/backend/models.py
+++ project/backend/models.py
@@ -49,7 +49,7 @@ class Bookmark(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     url = models.URLField(verbose_name='URL', max_length=200)
     title = models.CharField(max_length=200)
-    favicon = models.TextField(null=True)
+    favicon = models.TextField(blank=True)
     comments = models.TextField(max_length=500, blank=True)
     tags = models.ManyToManyField(Tag, related_name='bookmarks', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
